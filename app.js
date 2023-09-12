40f8421e61a81974bd4d0582b5d4f80d9cbe5050
Temitope-Afolabi <98202855+Temitope-Afolabi@users.noreply.github.com>
2023-09-12 20:59:57 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 const dateTime = new Date();
+const utcDate = dateTime.toISOString().split('.')[0] + 'Z';
+
 let day = weekday[dateTime.getDay()];
 
 app.get('/api', (req, res) => {
@@ -14,7 +16,7 @@ app.get('/api', (req, res) => {
     res.status(200).json({
         'slack_name': slack_name,
         'current_day': day,
-        'utc_time': dateTime,
+        'utc_time': utcDate,
         'track': track,
         'github_file_url': "https://github.com/Temitope-Afolabi/HNGInternship/blob/main/app.js",
         'github_repo_url': "https://github.com/Temitope-Afolabi/HNGInternship",
