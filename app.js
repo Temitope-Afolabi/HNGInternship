a3d8ab61d132c20b80d72e37e818673eb0c0afce
Temitope-Afolabi <98202855+Temitope-Afolabi@users.noreply.github.com>
2023-09-12 16:27:00 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const dateTime = new Date();
+let day = weekday[dateTime.getDay()];
+
+
+
+
+app.get('/api', (req, res) => {
+
+    const { slack_name, track } = req.query;
+    res.status(200).json({
+        'slack_name': slack_name,
+        'current_day': day,
+        'utc_time': dateTime,
+        'track': track,
+        'github_file_url': "https://github.com/username/repo/blob/main/file_name.ext",
+        'github_repo_url': "https://github.com/username/repo",
+        'status_code': 200
+    }
+    )
+})
+
+app.listen(5000, () => { console.log('Server is listening on port 5000...') })
